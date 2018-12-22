f539c69175200e94bb0a4a0c0d4ce226b8407a3c
Ankita Masand <amasand23@gmail.com>
2018-12-22 11:40:26 +0530

    separated index & background chunks

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PopupApp from './popup/index';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<PopupApp />, document.getElementById('root'));
+console.log('this is indexjs file')
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/popup/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Accordion from '../components/accordion'
+
+class PopupApp extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Hello gssgshsgssdjskdjskdjskdjsdkjdsh</h1>
+        <Accordion />
+        <Accordion />
+      </div>
+    )
+  }
+}
+
+export default PopupApp;
